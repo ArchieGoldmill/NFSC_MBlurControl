5d6f9960d43da7f36db2c5f008e835dd2755ba40
ARCHIE <ARCHIE@ARCHIE-PC>
2023-07-27 00:31:35 +0200

    Preserve blur

--- NFSC_MBlurControl/dllmain.cpp
+++ NFSC_MBlurControl/dllmain.cpp
@@ -2,142 +2,102 @@
 #include "includes/IniReader/IniReader.h"
 #include "includes/Injector/injector.hpp"
 
-auto CubicCameraMover_Update = (int(__thiscall*)(void*, float))0x004921B0;
+auto Game_CubicCameraMover_Update = (int(__thiscall*)(void*, float))0x004921B0;
+auto Game_DeltaTime = (float*)0x00A99A5C;
+auto Game_State = (int*)0x00A99BBC;
+auto Game_BlurUpdate = (BYTE*)0x0048249D;
+auto Game_IsPaused = (bool(__cdecl*)())0x004A62E0;
+auto Game_PhotoModeIn = (void(__thiscall*)(void*, int))0x005BBCC0;
+auto Game_PhotoModeOut = (void(__thiscall*)(void*, char))0x005C52F0;
 
-class BlurSettings
+struct BlurSettings
 {
-public:
 	float Start;
 	float End;
 	float Intensity;
-
-	BlurSettings() {}
-
-	BlurSettings(float start, float end, float intensity)
-	{
-		this->Start = start;
-		this->End = end;
-		this->Intensity = intensity;
-	}
-
-	BlurSettings operator-(BlurSettings& a)
-	{
-		return BlurSettings(this->Start - a.Start, this->End - a.End, this->Intensity - a.Intensity);
-	}
-
-	BlurSettings operator+(BlurSettings& a)
-	{
-		return BlurSettings(this->Start + a.Start, this->End + a.End, this->Intensity + a.Intensity);
-	}
-
-	BlurSettings operator/(float a)
-	{
-		return BlurSettings(this->Start / a, this->End / a, this->Intensity / a);
-	}
-
-	BlurSettings operator*(float a)
-	{
-		return BlurSettings(this->Start * a, this->End * a, this->Intensity * a);
-	}
-
-	void IncreaseBy(BlurSettings& step, BlurSettings& max)
-	{
-		this->Increase(this->Start, step.Start, max.Start);
-		this->Increase(this->End, step.End, max.End);
-		this->Increase(this->Intensity, step.Intensity, max.Intensity);
-	}
-
-	void DecreaseBy(BlurSettings& step, BlurSettings& min)
-	{
-		this->Decrease(this->Start, step.Start, min.Start);
-		this->Decrease(this->End, step.End, min.End);
-		this->Decrease(this->Intensity, step.Intensity, min.Intensity);
-	}
-
-private:
-
-	void Increase(float& val, float step, float max)
-	{
-		if (val < max)
-		{
-			val += step;
-		}
-		else
-		{
-			val = max;
-		}
-	}
-
-	void Decrease(float& val, float step, float min)
-	{
-		if (val > min)
-		{
-			val -= step;
-		}
-		else
-		{
-			val = min;
-		}
-	}
 };
 
 BlurSettings _Blur, Blur, NosBlur;
 float NosBlurTime;
-float* Game_DeltaTime = (float*)0x00A99A5C;
+float Time = 0;
 
-void __fastcall MainLoopTick(float** _this, int dummy, float a1)
+void __fastcall CubicCameraMover_Update(float** _this, int dummy, float a1)
 {
-	CubicCameraMover_Update(_this, a1);
+	Game_CubicCameraMover_Update(_this, a1);
 
-	bool isUsingNos = _this[0x24][0x38] && _this[0x24][0x24] > 0;
+	if (!Game_IsPaused())
+	{
+		bool isUsingNos = _this[0x24][0x38] && _this[0x24][0x24] > 0;
 
-	BlurSettings deltaPath = NosBlur - Blur;
-	BlurSettings velocity = deltaPath / NosBlurTime;
-	BlurSettings step = velocity * *Game_DeltaTime;
+		Time += (isUsingNos ? 1 : -1) * (*Game_DeltaTime) / NosBlurTime;
+		Time = std::clamp(Time, 0.0f, 1.0f);
 
-	isUsingNos ? _Blur.IncreaseBy(step, NosBlur) : _Blur.DecreaseBy(step, Blur);
+		_Blur.Start = std::lerp(Blur.Start, NosBlur.Start, Time);
+		_Blur.End = std::lerp(Blur.End, NosBlur.End, Time);
+		_Blur.Intensity = std::lerp(Blur.Intensity, NosBlur.Intensity, Time);
+	}
+}
+
+void __fastcall PhotoModeIn(void* _this, int, int a)
+{
+	if (*Game_State == 6)
+	{
+		*Game_BlurUpdate = 0x84;
+	}
+
+	Game_PhotoModeIn(_this, a);
+}
+
+void __fastcall PhotoModeOut(void* _this, int, char a)
+{
+	if (*Game_State == 6)
+	{
+		*Game_BlurUpdate = 0x85;
+	}
+
+	Game_PhotoModeOut(_this, a);
 }
 
 void Init()
 {
 	CIniReader iniReader("NFSCMBlurControl.ini");
 
-	Blur = BlurSettings(
-		iniReader.ReadFloat("BLUR", "Start", 350.0f),
-		iniReader.ReadFloat("BLUR", "End", 350.0f),
-		iniReader.ReadFloat("BLUR", "Intensity", 0.3f)
-	);
+	Blur.Start = iniReader.ReadFloat("BLUR", "Start", 350.0f);
+	Blur.End = iniReader.ReadFloat("BLUR", "End", 350.0f);
+	Blur.Intensity = iniReader.ReadFloat("BLUR", "Intensity", 0.3f);
 
-	NosBlur = BlurSettings(
-		iniReader.ReadFloat("NOS_BLUR", "Start", 350.0f),
-		iniReader.ReadFloat("NOS_BLUR", "End", 350.0f),
-		iniReader.ReadFloat("NOS_BLUR", "Intensity", 0.3f)
-	);
+	NosBlur.Start = iniReader.ReadFloat("NOS_BLUR", "Start", 350.0f);
+	NosBlur.End = iniReader.ReadFloat("NOS_BLUR", "End", 350.0f);
+	NosBlur.Intensity = iniReader.ReadFloat("NOS_BLUR", "Intensity", 0.3f);
 
-	NosBlurTime = iniReader.ReadFloat("NOS_BLUR", "Time", 300.0f);
+	NosBlurTime = iniReader.ReadFloat("NOS_BLUR", "Time", 1.0f);
 
 	_Blur = Blur;
 	injector::WriteMemory<float*>(0x00726467, &_Blur.End, true);
 	injector::WriteMemory<float*>(0x00726491, &_Blur.Start, true);
 	injector::WriteMemory<float*>(0x007265CB, &_Blur.Intensity, true);
 
-	injector::WriteMemory(0x009C7908, MainLoopTick, true);
+	injector::WriteMemory(0x009C7908, CubicCameraMover_Update, true);
 
-	if (iniReader.ReadInteger("CAMERA", "DisableGearChangeShake", 0) == 1)
+	if (iniReader.ReadInteger("GENERAL", "PhotoModeBlur", 0) == 1)
+	{
+		injector::MakeCALL(0x005BC646, PhotoModeIn, true);
+		injector::WriteMemory(0x009D4FBC, PhotoModeOut, true);
+		injector::WriteMemory<BYTE>(0x0048249D, 0x85, true); // Just to unprotect memmory
+	}
+
+	// Tanks to _Aero
+	if (iniReader.ReadInteger("GENERAL", "DisableGearChangeShake", 0) == 1)
 	{
 		unsigned char bytes[] = { 0xE9, 0xA6, 0x02, 0x00, 0x00, 0x90 };
 		injector::WriteMemoryRaw(0x00492A15, bytes, 6, true);
 	}
 
-	if (iniReader.ReadInteger("CAMERA", "DisableCameraDodge", 0) == 1)
+	if (iniReader.ReadInteger("GENERAL", "DisableCameraDodge", 0) == 1)
 	{
-		injector::WriteMemory<unsigned char>(0x00492F34, 0xEB, true);
+		injector::WriteMemory<unsigned char>(Game_BlurUpdate, 0xEB, true);
 	}
-
-	injector::MakeNOP(0x0073C359, 2, true);
-	injector::MakeNOP(0x0073C371, 2, true);
 }
-
 
 BOOL APIENTRY DllMain(HMODULE hModule, DWORD  ul_reason_for_call, LPVOID lpReserved)
 {
